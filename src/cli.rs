42bb91c9d9e61b0a4bd9bb47f888afc4e843e49f
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-04-18 16:05:43 +0700

    Update best hyperparameters

--- src/cli.rs
+++ src/cli.rs
@@ -201,7 +201,7 @@ pub enum Commands {
         fix_iteration: Option<usize>,
 
         /// The number of non-improved iterations before resetting the current solution = a2 * base
-        #[arg(long, default_value_t = 30.0)]
+        #[arg(long, default_value_t = 40.0)]
         reset_after_factor: f64,
 
         /// The maximum size of the elite set = a3
