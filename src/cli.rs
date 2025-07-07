03e412986da79be30ce02081c9017e2adfd5bd20
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-07-07 04:10:05 +0000

    Update hyperparameters

--- src/cli.rs
+++ src/cli.rs
@@ -210,11 +210,11 @@ pub enum Commands {
         fix_iteration: Option<usize>,
 
         /// The number of non-improved iterations before resetting the current solution = a2 * base
-        #[arg(long, default_value_t = 50.0)]
+        #[arg(long, default_value_t = 125.0)]
         reset_after_factor: f64,
 
         /// The maximum size of the elite set = a3
-        #[arg(long, default_value_t = 10)]
+        #[arg(long, default_value_t = 15)]
         max_elite_size: usize,
 
         /// Exponent value E attached to the cost function:
